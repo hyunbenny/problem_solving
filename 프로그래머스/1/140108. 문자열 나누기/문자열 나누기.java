8430b52bf982a53d00bbae46d01d575dac547ade
HYUNBIN CHO <127740003+hyunbenny@users.noreply.github.com>
2023-10-12 09:54:09 +0900

    [level 1] Title: 문자열 나누기, Time: 0.67 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -6,22 +6,14 @@ class Solution {
         int same = 0;
         int diff = 0;
 
-        for (int i = 0; i < s.length(); i++) {
+        for(int i = 0; i < s.length(); i++){
+            if(same == diff){
+                answer++;
+                ch = s.charAt(i);
+            }
+
             if(ch == s.charAt(i)) same++;
             else diff++;
-
-            if (same == diff) {
-                answer++;
-                same = 0;
-                diff = 0;
-
-                if(i + 1 < s.length()){
-                    ch = s.charAt(i + 1);
-                }
-
-            } else if (i + 1 == s.length()) {
-                answer++;
-            }
         }
 
         return answer;
