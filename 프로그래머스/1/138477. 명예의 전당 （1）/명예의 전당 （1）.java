5cc04bf068e45a374725295f9ef631b22dc232c2
HYUNBIN CHO <127740003+hyunbenny@users.noreply.github.com>
2023-10-25 16:51:58 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.30 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,29 +1,17 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 class Solution {
-   public int[] solution(int k, int[] scores) {
+    public int[] solution(int k, int[] scores) {
         int[] answer = new int[scores.length];
-        List<Integer> table = new ArrayList<>();
+
+        PriorityQueue<Integer> hallOfFame = new PriorityQueue<>();
 
         for (int i = 0; i < scores.length; i++) {
-            int score = scores[i];
-            if(i <= k - 1){
-                table.add(score);
-                int min = table.stream().mapToInt(n -> n).min().getAsInt();
-                answer[i] = min;
-            }else{
-                // 기존 table 데이터와 비교 -> table의 최소값 구하고 비교 후, 교체
-                int min = table.stream().mapToInt(n -> n).min().getAsInt();
-                if (score > min) {
-                    table.remove(table.indexOf(min));
-                    table.add(score);
-                }
-            }
-            table = table.stream().sorted((o1, o2) -> o2 - o1).collect(Collectors.toList());
-            answer[i] = table.stream().mapToInt(n -> n).min().getAsInt();
+            hallOfFame.add(scores[i]);
+
+            if(hallOfFame.size() > k) hallOfFame.poll();
+
+            answer[i] = hallOfFame.peek();
         }
 
 
