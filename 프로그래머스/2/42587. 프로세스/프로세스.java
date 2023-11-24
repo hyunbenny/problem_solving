d8f250c67afa70137073ab12f5d75b8f7dd0675f
HYUNBIN CHO <127740003+hyunbenny@users.noreply.github.com>
2023-11-24 09:04:42 +0900

    [level 2] Title: 프로세스, Time: 0.76 ms, Memory: 66 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int solution(int[] priorities, int location) {
+
+        int answer = 0;
+
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i : priorities) {
+            q.add(i);
+        }
+
+        while (!q.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == q.peek()) {
+                    q.poll();
+                    answer++;
+                    if(i == location) return answer;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
